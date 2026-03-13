12b023fb4bfb99edb81419f200386d8d6a283226
nadhirasya <nadhirasya@student.ub.ac.id>
2026-03-13 09:36:50 +0700

    Add files via upload

---
+++ app/Http/Middleware/ValidateBookYear.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class ValidateBookYear
+{
+    public function handle($request, Closure $next)
+    {
+        $year = $request->route('year');
+
+        if ($year) {
+            if ($year < 1900 || $year > date('Y')) {
+                return response()->json([
+                    'status' => 'error',
+                    'message' => 'Invalid book year'
+                ], 400);
+            }
+        }
+
+        return $next($request);
+    }
+}
